4662df47584bea2981c016e9b7970622b3d7bba5
Ina Oden Osterbo <inod5951@rackham1.uppmax.uu.se>
2018-04-23 10:16:10 +0200

    noticed error in file directory in assembly batch script.

--- code/1_genome_assembly.sh
+++ code/1_genome_assembly.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -A g2018003
 #SBATCH -p core
-#SBATCH -n 1
+#SBATCH -n 2
 #SBATCH -t 08:00:00
 #SBATCH -J 1_pb_assembly
 #SBATCH --mail-type=ALL
@@ -14,6 +14,6 @@ module load bioinfo-tools canu/1.7
 # Commands 
 canu \
 -p efaecium \
--d  /home/inod5951/GA_Project/data/raw_data/DNA/assembled_data \
+-d  /home/inod5951/GA_Project/data/assembled_data/DNA/PacBio/ \
 genomeSize=3m \
 -pacbio-raw /home/inod5951/GA_Project/data/raw_data/DNA/PacBio/*_Genome_PB_*.fastq.gz
