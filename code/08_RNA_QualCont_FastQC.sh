18c7d5ce3a36d04ad3607ce67bab1daa1ca3c380
Ina <ina.oden_osterbo.5951@student.uu.se>
2018-05-22 17:50:17 +0200

    Making sure it's all organized.

--- code/08_RNA_QualCont_FastQC.sh
+++ code/08_RNA_QualCont_FastQC.sh
@@ -4,10 +4,10 @@
 #SBATCH -p core
 #SBATCH -n 1
 #SBATCH -t 01:00:00
-#SBATCH -J 5_RNAtrimmed_QualCheck
+#SBATCH -J 3_RNAraw_QualCheck
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user ina.oden_osterbo.5951@student.uu.se
 
 module load bioinfo-tools FastQC
 
-fastqc /home/inod5951/GA_Project/data/trimmed_data/RNA/Illumina/1st/*_paired*
+fastqc /home/inod5951/GA_Project/data/raw_data/RNA/Illumina/*
